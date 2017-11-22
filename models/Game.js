65ecddb145e4ea15cdbd722fd3c97876f8da8f4f
cxjpxl <81886404@qq.com>
2017-11-22 23:59:20 +0800

    添加数据

---
+++ models/Game.js
@@ -0,0 +1,11 @@
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let GameSchema = new Schema({
+    nameH:{type:String,index:true}, //主队名字
+    nameG:{type:String,index:true},//客队名字
+    mid:{type:String,index:true},//比赛id
+    time:{type:String,index:true},//比赛时间
+},{collection:'game'});
+exports.Game = global.cxjDbCon.model('game',GameSchema);
