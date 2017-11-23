b3b8d5b10edd63febb09cca3da05fa19749e2597
cxjpxl <81886404@qq.com>
2017-11-23 11:59:37 +0800

    提交数据

--- routes/SendData/sendData.js
+++ routes/SendData/sendData.js
@@ -88,6 +88,17 @@ router.post('/sendData', async (ctx, next) => {
         let data = JSON5.parse(message);
         let mid = data["MID"]+"";
         let cid = data["CID"];
+
+        try{
+            cid = parseInt(cid);
+        }catch (e){
+            ctx.body={
+                no:203,
+                msg:'cid类型错误',
+            };
+            return ;
+        }
+
        if(cid==9926||cid==9927||cid==2086||cid==1062||cid==2055||cid==1031){
             let game = await Game.findOne({mid: mid}).exec();
             let time = new Date().getTime();
