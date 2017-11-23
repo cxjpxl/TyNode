5077bd915e7b5124de7b0395cacd6ccd3cbea211
cxjpxl <81886404@qq.com>
2017-11-23 12:24:22 +0800

    添加数据

--- routes/SendData/sendData.js
+++ routes/SendData/sendData.js
@@ -99,7 +99,6 @@ router.post('/sendData', async (ctx, next) => {
             return ;
         }
        if(cid==9926||cid==9927||cid==2086||cid==1062||cid==2055||cid==1031){
-           console.log("有数据过来:"+cid);
             let game = await Game.findOne({mid: mid}).exec();
             let time = new Date().getTime();
             if(game&&game.mid){
@@ -108,7 +107,6 @@ router.post('/sendData', async (ctx, next) => {
                     cid,
                     curTime:time,
                 };
-                console.log(curData);
                  for(let i = 0 ; i <  global.ctxs.length ; i ++){
                  let socket = global.ctxs[i];
                  if(!socket) continue;
