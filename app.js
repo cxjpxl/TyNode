85bfa8df3bc40a7ebc8b6c42113b22be1c2b5c18
[cxjpxl] <[81886404@qq.com]>
2018-03-05 11:04:19 +0800

    添加

--- app.js
+++ app.js
@@ -32,7 +32,16 @@ appWebSocket.ws.use((ctx) => {
             global.ctxs .push(ctx);
             console.log("有连接,当前连接个数"+global.ctxs.length);
         }*/
-        ctx.websocket.send("11");
+
+        try {
+            global.ws.server.clients.map(ws=>{
+                ws.send("15252525");
+            });
+        }catch (e){
+            console.log(e.toString());
+        }
+
+       ctx.websocket.send("11");
     });
     /* ctx.websocket.on('close', function(){
        for(let i= 0 ; i < global.ctxs.length ; i ++){
