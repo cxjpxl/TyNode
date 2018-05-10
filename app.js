3efef8891322a096b1795bdfb96ec7aa5994bfa2
[cxjpxl] <[81886404@qq.com]>
2018-05-10 19:27:14 +0800

    添加处理

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.use(session({
 TXinit("1106845374","hCcpXobCTUgSEDz0"); //识别码处理
 // 联赛名字，{1:主队进球,0:客队进球},主队比分,客队比分,主队名字,客队名字,比赛进行的时间
 acheck(1,100,(league,state,score1,score2,tm1,tm2,gametime)=>{
-    //console.log('--------',league,state,score1,score2,tm1,tm2,gametime)
+    console.log('--------',league,state,score1,score2,tm1,tm2,gametime)
 });
 
 /*******************路由*********************/
