c219b97aa979d05e7df8e690a402101004918113
[cxjpxl] <[81886404@qq.com]>
2018-07-17 10:49:48 +0800

    添加routes

--- routes/SendData/sendData.js
+++ routes/SendData/sendData.js
@@ -65,10 +65,9 @@ router.post('/sendData', async (ctx, next) => {
 
     if(type == 1){
         let dataArray = JSON5.parse(message);
-       //console.log(dataArray);
         if(dataArray.length > 0 ){
             //删除数据
-            await Game.remove({}).exec();
+          /*  await Game.remove({}).exec();
             //存数据
             for(let i = 0 ; i < dataArray.length; i++){
                 let dataItem = dataArray[i];
@@ -82,185 +81,194 @@ router.post('/sendData', async (ctx, next) => {
                 let time = dataItem["gameTime"];
                 let g = new Game({nameH,nameG,leagueName,mid,time});
                 await g.save();
-            }
+            }*/
+
+            global.biSaiArray = dataArray;
         }
     }
 
-    console.log("data",type,message);
     if(type == 2){
+        console.log("data",type,message);
         console.log("有事件发送过来!");
-        await update(Message,{time:new Date().getTime()},{$set:{
+      /*  await update(Message,{time:new Date().getTime()},{$set:{
             time:new Date().getTime(),
             message:message,
-        }});
+        }});*/
         let data = JSON5.parse(message);
         let mid = data["MID"]+"";
-        let cid = data["CID"];
-        try{
-            cid = parseInt(cid);
-        }catch (e){
+      //  let game = await Game.findOne({mid: mid}).exec();
+
+        if(!global.biSaiArray || global.biSaiArray.length == 0){
             ctx.body={
-                no:203,
-                msg:'cid类型错误',
+                no:200,
+                msg:'成功',
             };
             return ;
         }
-       //if(cid==9926||cid==9927||cid==2086||cid==1062||cid==2055||cid==1031){
-            let game = await Game.findOne({mid: mid}).exec();
+
+        let biSaiCatchs = JSON.parse(JSON.stringify(global.biSaiArray));//复制一份数据   双缓存
+        let game = null;
+        for(let i = 0 ; i < biSaiCatchs.length; i++){
+            let dataItem = biSaiCatchs[i];
+            let teamName = dataItem["teamName"];
+            let myMid = dataItem["idset"][0]+"";
+            if(myMid == mid){
+                game ={};
+                game.nameH = teamName[0];
+                game.nameG = teamName[1];
+                game.leagueName = dataItem["leagueName"];
+                game.mid = myMid;
+                game.time = dataItem["gameTime"];
+                break;
+            }
+        }
+        biSaiCatchs = null; //释放数据
         let time = new Date().getTime();
-            if(game&&game.mid){
-
-                console.log("主队:"+game.nameH+"||客队："+ game.nameG);
-                if(global.lianSaiData){
-                   if(game.nameH){
-                       if(game.nameH.indexOf("(中)")>0){
-                           game.nameH = game.nameH.replace("(中)","").trim();
-                       }
-
-                       if(game.nameH.indexOf("[中]")>0){
-                           game.nameH = game.nameH.replace("[中]","").trim();
-                       }
-
-                       if(game.nameH.indexOf("(女)")>0){
-                           game.nameH = game.nameH.replace("(女)","").trim();
-                       }
-
-                       if(game.nameH.indexOf("(后)")>0){
-                           game.nameH = game.nameH.replace("(后)","").trim();
-                       }
-
-                       if(game.nameH.indexOf("II")>0){
-                           game.nameH = game.nameH.replace("II","").trim();
-                       }
-
-                       if(game.nameH.indexOf("(W)")>0){
-                           game.nameH = game.nameH.replace("(W)","").trim();
-                       }
-
-                       if(game.nameH.indexOf("(w)")>0){
-                           game.nameH = game.nameH.replace("(w)","").trim();
-                       }
-
-                       if(game.nameH.indexOf("(R)")>0){
-                           game.nameH = game.nameH.replace("(R)","").trim();
-                       }
-
-
-                       game.nameH = game.nameH.trim();
-
-                       let nameHs = game.nameH.split("U");
-                       let isNum   = false;
-                       let numH = "";
-                       if(nameHs.length > 1){
-                           numH = nameHs[nameHs.length -1];
-                           try{
-                               numH = parseInt(numH);
-                               isNum = true;
-                           }catch (e){
-
-                            }
-                       }
-                       if(isNum){
-                           game.nameH = game.nameH.replace("U"+numH,"").trim();
-                       }
-                       if(global.lianSaiData[game.nameH+""]){
-                           game.nameH = global.lianSaiData[game.nameH+""];
-                       }
-                   }
-
-                    //客队
-                    if(game.nameG){
-                        if(game.nameG.indexOf("(中)")>0){
-                            game.nameG = game.nameG.replace("(中)","").trim();
-                        }
-                        if(game.nameG.indexOf("[中]")>0){
-                            game.nameG = game.nameG.replace("[中]","").trim();
-                        }
-
-                        if(game.nameG.indexOf("(女)")>0){
-                            game.nameG = game.nameG.replace("(女)","").trim();
-                        }
-
-                        if(game.nameG.indexOf("(后)")>0){
-                            game.nameG = game.nameG.replace("(后)","").trim();
-                        }
-
-                        if(game.nameG.indexOf("II")>0){
-                            game.nameG = game.nameG.replace("II","").trim();
-                        }
-
-                        if(game.nameG.indexOf("(W)")>0){
-                            game.nameG = game.nameG.replace("(W)","").trim();
-                        }
-
-                        if(game.nameG.indexOf("(w)")>0){
-                            game.nameG = game.nameG.replace("(w)","").trim();
-                        }
-
-                        if(game.nameG.indexOf("(R)")>0){
-                            game.nameG = game.nameG.replace("(R)","").trim();
-                        }
-
-                        game.nameG = game.nameG.trim();
-
-                        let nameGs = game.nameG.split("U");
-                        let isNum   = false;
-                        let numG = "";
-                        if(nameGs.length > 1){
-                            numG = nameGs[nameGs.length -1];
-                            try{
-                                numG = parseInt(numG);
-                                isNum = true;
-                            }catch (e){
-
-                            }
-                        }
-                        if(isNum){
-                            game.nameG = game.nameG.replace("U"+numG,"").trim();
-                        }
-
-                        if(global.lianSaiData[game.nameG+""]){
-                            game.nameG = global.lianSaiData[game.nameG+""];
-                        }
+        if(game&&game.mid){
+            console.log("主队:"+game.nameH+"||客队："+ game.nameG);
+            if(global.lianSaiData){
+                if(game.nameH){
+                    if(game.nameH.indexOf("(中)")>0){
+                        game.nameH = game.nameH.replace("(中)","").trim();
                     }
 
+                    if(game.nameH.indexOf("[中]")>0){
+                        game.nameH = game.nameH.replace("[中]","").trim();
+                    }
+
+                    if(game.nameH.indexOf("(女)")>0){
+                        game.nameH = game.nameH.replace("(女)","").trim();
+                    }
+
+                    if(game.nameH.indexOf("(后)")>0){
+                        game.nameH = game.nameH.replace("(后)","").trim();
+                    }
+
+                    if(game.nameH.indexOf("II")>0){
+                        game.nameH = game.nameH.replace("II","").trim();
+                    }
+
+                    if(game.nameH.indexOf("(W)")>0){
+                        game.nameH = game.nameH.replace("(W)","").trim();
+                    }
+
+                    if(game.nameH.indexOf("(w)")>0){
+                        game.nameH = game.nameH.replace("(w)","").trim();
+                    }
+
+                    if(game.nameH.indexOf("(R)")>0){
+                        game.nameH = game.nameH.replace("(R)","").trim();
+                    }
+
+
+                    game.nameH = game.nameH.trim();
+
+                    let nameHs = game.nameH.split("U");
+                    let isNum   = false;
+                    let numH = "";
+                    if(nameHs.length > 1){
+                        numH = nameHs[nameHs.length -1];
+                        try{
+                            numH = parseInt(numH);
+                            isNum = true;
+                        }catch (e){
+
+                        }
+                    }
+                    if(isNum){
+                        game.nameH = game.nameH.replace("U"+numH,"").trim();
+                    }
+                    if(global.lianSaiData[game.nameH+""]){
+                        game.nameH = global.lianSaiData[game.nameH+""];
+                    }
                 }
-                let curData = {
-                    cmd:1,
-                    game,
-                    data,
-                    curTime:time,
-                };
-                console.log("准备发送前:",curData);
-               if(global.ws&& global.ws.server&& global.ws.server.clients){
-                   console.log("准备发送！");
-                   try {
-                       global.ws.server.clients.forEach(ws=>{
-                           try {
-                               if(ws){
-                                  /* if(ws.myTag){
-                                       console.log("准备人:",ws.myTag);
-                                   }*/
-                                   ws.send(JSON.stringify(curData));
-                               }
-                           }catch (e1){
-                              /* if(ws && ws.myTag){
-                                   console.log("没有连接:",ws.myTag);
-                               }*/
-                               console.log(e1.toString());
-                           }
-                       });
-                   }catch (e){
-                       console.log(e.toString());
-                   }
-               }else{
-                   console.log("webSocket 对象连接找不到!");
-               }
+
+                //客队
+                if(game.nameG){
+                    if(game.nameG.indexOf("(中)")>0){
+                        game.nameG = game.nameG.replace("(中)","").trim();
+                    }
+                    if(game.nameG.indexOf("[中]")>0){
+                        game.nameG = game.nameG.replace("[中]","").trim();
+                    }
+
+                    if(game.nameG.indexOf("(女)")>0){
+                        game.nameG = game.nameG.replace("(女)","").trim();
+                    }
+
+                    if(game.nameG.indexOf("(后)")>0){
+                        game.nameG = game.nameG.replace("(后)","").trim();
+                    }
+
+                    if(game.nameG.indexOf("II")>0){
+                        game.nameG = game.nameG.replace("II","").trim();
+                    }
+
+                    if(game.nameG.indexOf("(W)")>0){
+                        game.nameG = game.nameG.replace("(W)","").trim();
+                    }
+
+                    if(game.nameG.indexOf("(w)")>0){
+                        game.nameG = game.nameG.replace("(w)","").trim();
+                    }
+
+                    if(game.nameG.indexOf("(R)")>0){
+                        game.nameG = game.nameG.replace("(R)","").trim();
+                    }
+
+                    game.nameG = game.nameG.trim();
+
+                    let nameGs = game.nameG.split("U");
+                    let isNum   = false;
+                    let numG = "";
+                    if(nameGs.length > 1){
+                        numG = nameGs[nameGs.length -1];
+                        try{
+                            numG = parseInt(numG);
+                            isNum = true;
+                        }catch (e){
+
+                        }
+                    }
+                    if(isNum){
+                        game.nameG = game.nameG.replace("U"+numG,"").trim();
+                    }
+
+                    if(global.lianSaiData[game.nameG+""]){
+                        game.nameG = global.lianSaiData[game.nameG+""];
+                    }
+                }
+
             }
-            else{
-                console.log("找不到比赛队伍！！！");
+            let curData = {
+                cmd:1,
+                game,
+                data,
+                curTime:time,
+            };
+            console.log("准备发送前:",curData);
+            if(global.ws&& global.ws.server&& global.ws.server.clients){
+                console.log("准备发送！");
+                try {
+                    global.ws.server.clients.forEach(ws=>{
+                        try {
+                            if(ws){
+                                ws.send(JSON.stringify(curData));
+                            }
+                        }catch (e1){
+                            console.log(e1.toString());
+                        }
+                    });
+                }catch (e){
+                    console.log(e.toString());
+                }
+            }else{
+                console.log("webSocket 对象连接找不到!");
             }
-      // }
+        }
+        else{
+            console.log("找不到比赛队伍！！！");
+        }
     }
 
 
