8290886fe4602acb6c058678a3d450f3ceb86c7d
[cxjpxl] <[81886404@qq.com]>
2018-08-24 12:25:27 +0800

    更改版本号

--- routes/login/login.js
+++ routes/login/login.js
@@ -156,9 +156,9 @@ router.post('/login',async (ctx,next)=>{
     let comId = body.comId;
 
     let version = body.version;
-    let v = "V2.90"; //最新版本信息
+    let v = "V3.0"; //最新版本信息
 
-    let v1 = "V2.90";
+    let v1 = "V3.0";
 
     //如果版本号不存在或者不是当前服务器对应的版本 不能使用
     if(!version ||(version!=v1 && version!=v) ){
